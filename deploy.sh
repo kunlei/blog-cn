d568f5dc8e0245ca0e573f5bc2ae52005e6442b3
Kunlei Lian <kunlei.lian@walmart.com>
2019-12-30 15:10:01 -0600

    update

---
+++ deploy.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# If a command fails then the deploy stops
+set -e
+
+printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+
+# Build the project.
+hugo -t beautifulhugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+
+# Go To Public folder
+cd public
+
+# Add changes to git.
+git add .
+
+# Commit changes.
+msg="rebuilding site $(date)"
+if [ -n "$*" ]; then
+	msg="$*"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
+
+# come back to project root
+cd ..
+
+git add .
+git commit -m "update"
+git push origin master
